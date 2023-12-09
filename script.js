82c3c8279c3813b76cdfe8d3cdeb48b8e1dfd076
codezalez <egonzalezla95@gmail.com>
2023-12-09 18:24:20 -0300

    Adding a score function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ['rock', 'paper', 'scissor']
+let computerScore = 0
+let playerScore = 0
 
 let getComputerChoice = () => {
     const randomIndex = Math.floor(Math.random() * choices.length)
@@ -6,25 +8,35 @@ let getComputerChoice = () => {
 }
 
 let computerSelection = getComputerChoice()
-console.log('Computer choice: ', computerSelection)
+console.log(`Computer Choice: ${computerSelection}`)
+
+let updateScores = (result) => {
+    if (result === "You win") {
+        playerScore++
+    } else if (result === "You loose") {
+        computerScore++
+    }
+}
 
 let game = () => {
     let playerSelection = prompt('Choose rock, paper or scissor: ')
-    console.log('Player choice: ', playerSelection)
+    console.log(`Player Choice: ${playerSelection}`)
+    
     if (playerSelection.toLowerCase() === computerSelection) {
         return "It's a draw"
-    }
+    }getComputerChoice()
 
     if (playerSelection.toLowerCase() === 'rock') {
-        return computerSelection === 'scissor' ? "You win" : "You loose"
+        return computerSelection === 'scissor' ? updateScores("You win") : updateScores("You loose")
     } else if (playerSelection.toLowerCase() === 'paper') {
-        return computerSelection === 'rock' ? "You win" : "You loose"
+        return computerSelection === 'rock' ? updateScores("You win") : updateScores("You loose")
     } else if (playerSelection.toLowerCase() === 'scissor') {
-        return computerSelection === 'paper' ? "You win" : "You loose"
+        return computerSelection === 'paper' ? updateScores("You win") : updateScores("You loose")
     } else {
         return "Wrong input"
     }
 }
 
-let result = game()
-console.log('Result: ', result)
+game()
+console.log(`Player Score: ${playerScore}`)
+console.log(`Computer Score: ${computerScore}`)
