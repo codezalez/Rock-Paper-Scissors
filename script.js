720c6b92531f613abfa8a85e3d438ee9fe1557b5
codezalez <egonzalezla95@gmail.com>
2023-12-09 17:24:11 -0300

    Basic game function added

--- script.js
+++ script.js
@@ -5,5 +5,26 @@ let getComputerChoice = () => {
     return choices[randomIndex]
 }
 
-let computerChoice = getComputerChoice()
-console.log('Computer choose: ',computerChoice)
+let computerSelection = getComputerChoice()
+console.log('Computer choice: ', computerSelection)
+let playerSelection = prompt('Choose rock, paper or scissor: ')
+console.log('Player choice: ', playerSelection)
+
+let game = () => {
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return "It's a draw"
+    }
+
+    if (playerSelection.toLowerCase() === 'rock') {
+        return computerSelection === 'scissor' ? "You win" : "You loose"
+    } else if (playerSelection.toLowerCase() === 'paper') {
+        return computerSelection === 'rock' ? "You win" : "You loose"
+    } else if (playerSelection.toLowerCase() === 'scissor') {
+        return computerSelection === 'paper' ? "You win" : "You loose"
+    } else {
+        return "Wrong input"
+    }
+}
+
+let result = game()
+console.log('Result: ', result)
