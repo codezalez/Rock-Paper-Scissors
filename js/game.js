3e21f1e7fef6dd9753d3148d72da649fe3de9965
codezalez <egonzalezla95@gmail.com>
2024-01-03 18:44:00 -0300

    The game ends at five points

--- js/game.js
+++ js/game.js
@@ -1,6 +1,5 @@
-//import { choices } from './choices.js';
 import { getComputerChoice } from './computer.js';
-import { updateScores, displayScores, computerScore, playerScore } from './scores.js';
+import { updateScores, displayScores, playerScore, computerScore } from './scores.js';
 
 export const runGame = () => {
     const buttons = document.querySelectorAll('button');
@@ -8,24 +7,34 @@ export const runGame = () => {
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            const playerSelection = button.id;
-            const computerSelection = getComputerChoice()
-
-            if (playerSelection === computerSelection) {
-                resultElement.textContent = "It's a draw";
-            } else if (
-                (playerSelection === 'rock' && computerSelection === 'scissor') ||
-                (playerSelection === 'paper' && computerSelection === 'rock') ||
-                (playerSelection === 'scissors' && computerSelection === 'paper')
-            ) {
-                updateScores("You win!");
-                resultElement.textContent = 'You win!'
-            } else {
-                updateScores("You lose!");
-                resultElement.textContent = 'You lose!'
+            if (playerScore < 5 && computerScore < 5) {
+                const playerSelection = button.id;
+                const computerSelection = getComputerChoice()
+    
+                if (playerSelection === computerSelection) {
+                    resultElement.textContent = "It's a draw";
+                } else if (
+                    (playerSelection === 'rock' && computerSelection === 'scissor') ||
+                    (playerSelection === 'paper' && computerSelection === 'rock') ||
+                    (playerSelection === 'scissors' && computerSelection === 'paper')
+                ) {
+                    updateScores("You win!");
+                    resultElement.textContent = 'You win!'
+                } else {
+                    updateScores("You lose!");
+                    resultElement.textContent = 'You lose!'
+                }
+            
+                displayScores();
             }
-        
-            displayScores();
+            if (playerScore === 5 || computerScore === 5) {
+                endGame();
+            }
         })
     });
+};
+
+export const endGame = () => {
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = playerScore > computerScore ? "You have won the game!" : "You have lost the game!";
 };
